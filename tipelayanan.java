dd3dc3447cd84a99b94e55b52ae0ab8ba4942678
FazaDinanM <109134939+FazaDinanM@users.noreply.github.com>
2022-07-12 14:20:50 +0700

    Add files via upload

---
+++ tipelayanan.java
@@ -0,0 +1,14 @@
+package com.zhafhira.jejaring_marketer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class tipelayanan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tipelayanan);
+    }
+}
